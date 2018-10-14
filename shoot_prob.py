3ea7c45e2a50b791ce8a4208c6c0aa49860f1335
Hao He <haohe@Haos-MacBook-Air.local>
2018-10-14 11:21:49 +0800

    beta v0.1 for shoot_prob

--- shoot_prob.py
+++ shoot_prob.py
@@ -2,37 +2,52 @@
 # coding: utf-8
 
 import requests
-#import time
 import re
-#import random
-#import math
-#import os
-#import pickle
-#from bs4 import BeautifulSoup
+import sys
 
-'''url_search_base = 'http://songsearch.kugou.com/song_search_v2?callback=jQuery112406335936274624496_1539417511214&keyword='
 
-keyword = '一百万个可能'
+url_search_base = 'http://songsearch.kugou.com/song_search_v2?callback=jQuery112406335936274624496_1539417511214&keyword='
+hash_url = 'http://www.kugou.com/yy/index.php?r=play/getdata&hash='
+
+
+
+
+keyword = input("歌曲名: ")
 
 response = requests.get(url_search_base+keyword)
-print (response.status_code)
-print(response.headers)
+
 print('----------------')
 
-songname = re.findall('"SongName":"(.*?)"',response.text)
+songname = re.findall('"FileName":"(.*?)"',response.text)
 index = 0
 for song in songname:
 	print ('[%d]:%s' %(index,song))
 	index+=1
-#print(response.text)'''
 
-url_test = 'http://fs.w.kugou.com/201810131617/ab71285a7be0cbc925202997726efad8/G137/M02/03/15/yQ0DAFuaCCGAQmMwADsktht-moM154.mp3'
+files = re.findall(r'"FileHash":"(.*?)"',response.text)
 
-with open('test_song.mp3','wb') as f:
-	f.write(requests.get(url_test).content)
-print ("downloaded")
+num = int(input("请输入序号: "))
 
 
+hash_response = requests.get(hash_url+files[num])
 
+play_url = re.findall(r'"play_url":"(.*?)"',hash_response.text)
+download_url = re.sub(r'\\',r'',play_url[0])
+
+
+music = requests.get(download_url, stream=True)
+total_size = music.headers['Content-Length']
+chunk_size=1024
+rate = chunk_size / int(total_size) * 100
+counter = 1
+
+with open(songname[0]+'.mp3','wb') as f:
+	for chunk in music.iter_content(chunk_size=chunk_size):
+		if chunk:
+			f.write(chunk)
+			print ('%d%%' %int(rate*counter),end = '\r')
+			counter+=1
+
+print('\ndone') 
 
 
